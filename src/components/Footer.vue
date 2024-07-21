5e74f115f3282338d8e8b582261c2e7ecf2f28c4
James Kalu <107466930+Jamesiukalu@users.noreply.github.com>
2024-07-21 18:02:21 -0400

    AppData

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <p>Copyright &copy; 2024</p>
+        <router-link to="/about">About</router-link>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterItem'
+}
+</script>
+
+<style>
+a {
+    color: #333
+}
+footer {
+    margin-top: 30px;
+    text-align: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <ButtonItem v-show="homepage" @btn-click="$emit('toggle-add-task')" 
+        :text="showAddTask ? 'Close' : 'Add Task' "
+        :color="showAddTask ? 'red' : 'green' " />
+    </header>
+</template>
+
+<script>
+import ButtonItem from './Button'
+export default {
+    name: 'HeaderItem',
+    props: {
+        title: String,
+        showAddTask:Boolean
+    },
+    components: {
+        ButtonItem,
+    },
+    computed: {
+        homepage() {
+            if (this .$route.path === '/') {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
